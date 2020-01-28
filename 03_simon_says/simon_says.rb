3a36b8c197e1d432577b653f01702b4abc0c67c4
wseider <wseider@salsify.com>
2020-01-28 14:23:30 -0500

    fun w strings and some array action too

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,32 @@
 #write your code here
+def echo(input)
+    input
+end 
+
+def shout(input)
+    input.upcase
+end 
+
+def repeat(input, num=2)
+    ("#{input} " * num).rstrip
+end 
+
+def start_of_word(word, num)
+    word.slice(/^\w{#{num}}/)
+end 
+
+def first_word(string)
+    string.partition(" ").first
+end 
+
+def titleize(input)
+    #input.split.map(&:capitalize).join(' ') works for non little word case, cool method
+    array = input.split(' ')
+    array.each do |word|
+        if (word != 'and' && word != 'the' && word != 'or' && word != 'over') 
+            word.capitalize!
+        end   
+        end
+        array.first.capitalize!
+        array.join(' ')
+end 
